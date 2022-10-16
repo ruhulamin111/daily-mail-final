ccb299b9751d204cbaf991b3ea34efdb035acdf0
Hasan Shanto <hasanshanto922@gmail.com>
2022-10-16 23:01:07 +0600

    home page design

---
+++ components/Header.js
@@ -0,0 +1,51 @@
+import styles from '../styles/Header.module.css'
+import MenuIcon from '@mui/icons-material/Menu';
+// import ContactMailIcon from '@mui/icons-material/ContactMail';
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AppsIcon from '@mui/icons-material/Apps';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Image from 'next/image';
+import { Avatar, IconButton } from '@mui/material';
+
+function Header() {
+    return (
+        <div className={styles.header}>
+    
+          <div className={styles.header_left}>
+            <IconButton>
+              <MenuIcon />
+              <img src = "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_1x_r2.png" 
+                alt = "logo"
+                />
+            </IconButton>
+            
+          </div>
+    
+          <div className={styles.header_middle}>
+            <SearchIcon />
+            <input placeholder='Search your mail' type="text" />
+            <ArrowDropDownIcon />
+          </div>
+    
+          <div className={styles.header_right}>
+    
+            <IconButton>
+              <AppsIcon />
+            </IconButton>
+            <IconButton>
+              <SettingsIcon />
+            </IconButton>
+            <IconButton>
+              <Avatar />
+            </IconButton>
+            <IconButton>
+              {/* <LogoutIcon onClick={() => signOut()} /> */}
+            </IconButton>
+          </div>
+        </div>
+      )
+    }
+    
+
+export default Header
---
+++ components/Mail.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import styles from '../styles/Mail.module.css'
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import KeyboardIcon from '@mui/icons-material/Keyboard';
+import InboxIcon from '@mui/icons-material/Inbox';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import InfoIcon from '@mui/icons-material/Info';
+import ForumIcon from '@mui/icons-material/Forum';
+
+
+function Mail() {
+  return (
+    <div className ={styles.mail}>
+            <div className ={styles.mail__containerTop}>
+            <div className ={styles.mailTop}>
+                <CheckBoxIcon className ={styles.checkbox} />
+                <ArrowDropDownIcon />
+                <RefreshIcon />
+                <MoreVertIcon />
+            </div>  
+            <div className ={styles.mailTopRight}>
+                <ChevronLeftIcon />
+                <ChevronRightIcon />
+                <KeyboardIcon />
+                <ArrowDropDownIcon />
+            </div>
+            </div>
+            <div className ={styles.mail__containerMid}>
+                <div className ={styles.mail__containerMidOptions}>
+                    <div className ={styles.mail__containerMidOption}>
+                        <InboxIcon />
+                        <h3>Primary</h3>
+                    </div>
+                    <div className ={styles.mail__containerMidOption}>
+                        <PeopleAltIcon />
+                        <h3>Social</h3>
+                    </div>
+                    <div className ={styles.mail__containerMidOption}>
+                        <LocalOfferIcon />
+                        <h3>Promotions</h3>
+                    </div>
+                    <div className ={styles.mail__containerMidOption}>
+                        <InfoIcon />
+                        <h3>Updates</h3>
+                    </div>
+                    <div className ={styles.mail__containerMidOption}>
+                        <ForumIcon />
+                        <h3>Forums</h3>
+                    </div>
+                </div>
+            </div>
+            <div className ={styles.mail__containerMain}>
+                <div className ={styles.mail__containerMainCards}>
+                    {/* <MailCard /> */}
+                </div>
+            </div>
+         </div>
+  )
+}
+
+export default Mail
